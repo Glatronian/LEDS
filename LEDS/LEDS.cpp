4a3da5384d08da9ef639de093ae1c83813640c96
uaczr <uaczr@student.kit.edu>
2015-04-14 08:54:27 +0200

    Libraries

--- LEDS/LEDS.cpp
+++ LEDS/LEDS.cpp
@@ -17,14 +17,18 @@ void setup(){
 	digitalWrite(13, LOW);
 	Serial.begin(115200);
 	CONTROLLER.init(fleds, PARAMETER, RECEIVEBUFFER);
+#ifdef WS2811
 	FastLED.addLeds<WS2811, DATA_PIN, BRG>(fleds, NUM_LEDS);
 	FastLED.setCorrection(0xFF6F7F);
 	FastLED.show();
+#endif
+
 }
 
 void loop() {
 	CONTROLLER.communication();
 	CONTROLLER.caller();
+#ifdef WS2811
 	FastLED.show();
-
+#endif
 }

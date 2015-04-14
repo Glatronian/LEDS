480f364891aab1101a8b539116b58ccd1833a01f
uaczr <uaczr@student.kit.edu>
2015-04-14 12:07:46 +0200

    Reinit

---
+++ LEDS/LEDS.h
@@ -0,0 +1,43 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+
+#ifndef _LEDS_H_
+#define _LEDS_H_
+
+
+
+//add your includes for the project Blitzdings here
+
+#include <Arduino.h>
+#include <FastLED.h>
+//#ifdef LPD6803_DEF
+#include <LPD6803.h>
+#include <TimerOne.h>
+//#endif
+#include "controller.h"
+#include "defines.h"
+//end of add your includes here
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project Blitzdings here
+
+
+
+
+//Do not add code below this line
+#endif /* _Blitzdings_H_ */

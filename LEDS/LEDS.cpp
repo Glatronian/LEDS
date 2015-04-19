dbcb7c3a0efe75e9b24478b4aa14a9d40e32a01d
uaczr <uaczr@student.kit.edu>
2015-04-19 14:30:43 +0200

    blöd

--- LEDS/LEDS.cpp
+++ LEDS/LEDS.cpp
@@ -1,51 +1,15 @@
+// Do not remove the include below
 #include "LEDS.h"
-#define WS8211
-using namespace leds;
-
-//globale Variabeln
-
-// Define the array of leds
-CRGB fleds[NUM_LEDS];
-uint8_t PARAMETER[232];
-uint8_t RECEIVEBUFFER[64];
-leds::controller CONTROLLER;
-#ifdef LPD6803_DEF
-LPD6803 strip = LPD6803(NUM_LEDS, DATA_PIN, CLOCK_PIN);
-CRGB* ptr;
-#endif
 
 
-void setup(){
-	//controller.initController();
-	pinMode(13, OUTPUT);
-	digitalWrite(13, LOW);
-	Serial.begin(115200);
-	CONTROLLER.init(fleds, PARAMETER, RECEIVEBUFFER);
-
-#ifdef WS2811_DEF
-	FastLED.addLeds<WS2811, DATA_PIN, BRG>(fleds, NUM_LEDS);
-	FastLED.setCorrection(0xFF6F7F);
-	FastLED.show();
-#endif
-#ifdef LPD6803_DEF
-strip.setCPUmax(50);
-strip.begin();
-strip.show();
-#endif
-
+//The setup function is called once at startup of the sketch
+void setup()
+{
+// Add your initialization code here
 }
 
-void loop() {
-	CONTROLLER.communication();
-	CONTROLLER.caller();
-#ifdef WS2811_DEF
-	FastLED.show();
-#endif
-#ifdef LPD6803_DEF
-	ptr = fleds;
-	for(uint8_t i = 0; i < NUM_LEDS; i++){
-		strip.setPixelColor(i, ptr->r, ptr->g, ptr->b);
-	}
-	strip.show();
-#endif
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
 }
